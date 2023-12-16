46f79d9fa52deb96c558bb7a5f0d80a04166f141
Kelvin <kkuria383@yahoo.com>
2023-12-16 12:21:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solutions in this file
+
+function countDown(startingNumber) {
+
+    if (typeof startingNumber !== 'number' || startingNumber <= 0 || !Number.isInteger(startingNumber)) {
+      console.log("Please provide a positive integer as input for countDown function.");
+      return;
+    }
+  
+
+    while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber--;
+    }
+  }
+  function writeCards(namesArray) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < namesArray.length; i++) {
+      thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful surprise gift!`);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  countDown(10);
+  
+:
+  const names = ["Guadalupe", "Ollie", "Aki"];
+  const messages = writeCards(names);
+  console.log(messages);
+  
+
+  
